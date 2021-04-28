f0e394365d14056e763cd8522e9ba3928812552d
stefa2k <stefan.kobrc@rocklogic.at>
2021-04-28 16:13:24 +0200

    basic navigation

---
+++ frontend/src/components/cc/home/Home.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <span> This is the home screen, please make me pretty </span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Home",
+  components: {},
+  data() {
+    return {
+    };
+  },
+  props: {
+    model: Object,
+  },
+};
+</script>
+
+<style scoped></style>
---
+++ frontend/src/components/cc/updates/Updates.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <span> This is the updates screen, please make me pretty </span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Updates",
+  components: {},
+  data() {
+    return {
+    };
+  },
+  props: {
+    model: Object,
+  },
+};
+</script>
+
+<style scoped></style>
