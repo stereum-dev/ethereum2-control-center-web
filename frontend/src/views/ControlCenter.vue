be8937da15f58feb7c8ae3a1b1e8afb8e0e46f62
stefa2k <stefan.kobrc@rocklogic.at>
2021-04-28 15:24:25 +0200

    control-center routing & dummy navbar

---
+++ frontend/src/views/ControlCenter.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="control-center">
+    <control-center-overview :model="model" />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import ControlCenterOverview from "@/components/ControlCenterOverview.vue";
+
+export default {
+  name: "ControlCenter",
+  components: {
+    ControlCenterOverview,
+  },
+  data() {
+    return {
+      model: {
+        network: "pyrmont",
+        client: "allbeacons",
+        installationFolder: "/opt/stereum",
+        override: "default",
+        eth1nodes: [],
+        updates: {},
+      },
+    };
+  },
+};
+</script>
