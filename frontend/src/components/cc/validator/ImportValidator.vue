a919e83a43481133b0770e71b4504b1046eb899e
gbayasgalan <bayasgalan.gantumur@rocklogic.at>
2021-05-07 14:40:33 +0200

    validator import improvement

--- frontend/src/components/cc/validator/ImportValidator.vue
+++ frontend/src/components/cc/validator/ImportValidator.vue
@@ -1,68 +1,90 @@
 <template>
   <div id="app" @dragover.prevent @drop.prevent>
-    <div
-      class="container mt-3 pt-3"
-      @dragover="fileDragIn"
-      @drop="handleFileDrop"
-    >
-      <b-form-group
-        label="Add your validator keys here:"
-        label-for="input-1"
-        label-size="m"
+    <div class="container" @drop="fileDragOut">
+      <div class="row pb-3 pt-3">
+        <h2>Import</h2>
+      </div>
+
+      <div
+        class="file-wrapper"
+        @dragleave="fileDragOut"
+        @dragover="fileDragIn"
+        @drop="handleFileDrop"
+        v-bind:style="{ 'background-color': color }"
       >
-        <div class="file-wrapper">
-          <input
-            type="file"
-            name="file-input"
-            multiple="True"
-            @change="handleFileInput"
-            title="Put your key files here!"
-          />
-          Click or drag to insert key files
+        <input
+          type="file"
+          name="file-input"
+          multiple="true"
+          @change="handleFileInput"
+          title="Put your key files here!"
+        />
+        Click or drag to insert key files
+      </div>
+
+      <div>
+        <table class="table table-striped table-hover">
+          <thead>
+            <tr>
+              <th>Number</th>
+              <th>Key file</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+
+          <tbody>
+            <tr v-for="(file, index) in files" v-bind:key="file && index">
+              <td>
+                {{ index + 1 }}
+              </td>
+
+              <td>
+                {{ file.name }}
+              </td>
+
+              <td>
+                <b-button
+                  size="sm"
+                  @click="removeFile(index)"
+                  variant="primary"
+                  class="mb-2 mr-sm-2 mb-sm-0"
+                  v-b-trash.hover
+                  title="remove file"
+                >
+                  <b-icon icon="trash" aria-hidden="true"></b-icon>
+                </b-button>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+
+        <div class="container pb-3 pt-3">
+          <p class="text-left">
+            <b-form-group
+              label="Validator password:"
+              label-for="input-1"
+              label-size="m"
+            >
+              <input
+                v-model="password"
+                type="password"
+                placeholder="min.8 characters"
+                title="Put key file's password here!"
+                align="left"
+              />
+            </b-form-group>
+
+            <b-button
+              variant="primary"
+              @click="importValidator()"
+              :disabled="files.length === 0 || password.length < 8"
+              title="import accounts"
+            >
+              Import
+            </b-button>
+          </p>
         </div>
-      </b-form-group>
-
-      <ul>
-        <li
-          class="key-files mt-2 pt-1"
-          v-for="(file, index) in files"
-          v-bind:key="file && index"
-        >
-          {{ file.name }}
-          <b-button
-            variant="primary"
-            style="height: 20px; padding: 0 4px; margin-left: 5px"
-            @click="removeFile(index)"
-            title="delete file"
-            >delete</b-button
-          >
-        </li>
-      </ul>
-    </div>
-
-    <b-form-group
-      label="Validator password:"
-      label-for="input-1"
-      label-size="m"
-    >
-      <input
-        v-model="password"
-        type="password"
-        placeholder="min.8 characters"
-        title="Put key file's password here!"
-      />
-    </b-form-group>
-
-    <div class="container">
-      <p class="text-center">
-        <b-button
-          variant="primary"
-          @click="importValidator()"
-          :disabled="files.length === 0 || password.length < 8"
-          title="import choosen accounts"
-          >Import</b-button
-        >
-      </p>
+      </div>
     </div>
   </div>
 </template>
@@ -70,11 +92,16 @@
 <script>
 export default {
   name: "ImportValidator",
-  data: function () {
+  components: {},
+  data() {
     return {
       files: [],
+      color: "#87cefa",
       password: "",
     };
+  },
+  props: {
+    ethereum2config: Object,
   },
   methods: {
     handleFileDrop(e) {
@@ -98,38 +125,28 @@ export default {
     fileDragIn() {
       this.color = "white";
     },
+    fileDragOut() {
+      this.color = "#87cefa";
+    },
     importValidator() {
-      //to do import
+      //todo import
     },
   },
 };
 </script>
 
 <style>
-body,
-html {
-  height: 100%;
-}
-
 #app {
   height: 100vh;
 }
-
-main {
-  margin-top: 30px;
-  height: 100%;
-}
-
 .file-wrapper {
   text-align: center;
-  margin: auto;
   width: 100%;
-  border: 3px solid #24a0ed;
-  padding: 10px;
+  border: 3px inset #24a0ed;
+  padding: 15px;
   position: relative;
   overflow: hidden;
 }
-
 .file-wrapper input {
   position: absolute;
   top: 0;
@@ -139,10 +156,5 @@ main {
   filter: alpha(opacity=0);
   font-size: 300px;
   height: 200px;
-}
-
-.key-files {
-  display: flex;
-  text-align: left;
 }
 </style>
