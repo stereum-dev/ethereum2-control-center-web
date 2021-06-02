c43ce0efcad1ad3318d4f193eac1fecd60d47ba0
gbayasgalan <bayasgalan.gantumur@rocklogic.at>
2021-06-02 13:49:25 +0200

    added features

---
+++ frontend/src/components/cc/miscellaneous/PruneGeth.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="container">
+    <div class="row pb-3 pt-3">
+      <h2>Prune Geth</h2>
+    </div>
+    <div class="col-12 text-left">
+      <span>It will delete several of your Ethereum 1 client - Geth's data to free up some storage space</span>
+    </div>
+    <div class="row">
+      <div class="col-12 text-right">
+        <b-button @click="gethPrune()" variant="primary">Prune Geth</b-button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "PruneGeth",
+  components: {},
+  data() {
+    return {};
+  },
+  props: {
+    model: Object,
+  },
+  methods: {
+    gethPrune() {
+      // todo
+    },
+  },
+};
+</script>
+
+<style scoped></style>
---
+++ frontend/src/components/cc/miscellaneous/Restart.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="container">
+    <div class="row pb-3 pt-3">
+      <h2>Restart</h2>
+    </div>
+    <div class="col-12 text-left">
+      <span>Your device will be restarted in couple of minutes!</span>
+    </div>
+    <div class="row">
+      <div class="col-12 text-right">
+        <b-button @click="Restart()" variant="primary">Restart</b-button>
+      </div>
+    </div>
+  </div>
+</template>
+
+
+<script>
+export default {
+  name: "Restart",
+  components: {},
+  data() {
+    return {};
+  },
+  props: {
+    model: Object,
+  },
+  methods: {
+    Restart() {
+      // todo
+    },
+  },
+};
+</script>
+
+<style scoped></style>
