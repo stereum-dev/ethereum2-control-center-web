cc850fdd61a1e7471e1f5a87a44fa946f7f27ada
stefa2k <stefan.kobrc@rocklogic.at>
2021-06-17 14:46:53 +0200

    wizard: validation

--- frontend/src/components/wizard/InstallationFolderTab.vue
+++ frontend/src/components/wizard/InstallationFolderTab.vue
@@ -5,7 +5,14 @@
       <b-form-input
         v-model="model.installationFolder"
         placeholder="Enter installation path"
+        :state="validation"
       ></b-form-input>
+      <b-form-invalid-feedback :state="validation">
+        Invalid path!
+      </b-form-invalid-feedback>
+      <b-form-valid-feedback :state="validation">
+        Looks like a valid path.
+      </b-form-valid-feedback>
     </div>
   </div>
 </template>
@@ -16,6 +23,12 @@ export default {
   components: {},
   props: {
     model: Object,
+  },
+
+  computed: {
+    validation() {
+      return this.model.installationFolder.length > 0 && this.model.installationFolder.startsWith("/");
+    },
   },
 };
 </script>
